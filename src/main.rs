a3a400f0ac162a0b1e99af03d5fdd09a7fa603b8
Sergey Fedorov <oni.strech@gmail.com>
2017-08-09 22:43:49 +0200

    Replace args from example to builder

--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,62 @@ extern crate clap;
 
 use curl::easy::{Easy, List};
 use serde_json::Value;
-use clap::App;
+use clap::{App, Arg};
 
 use std::fmt;
-use std::process::{self, ExitStatus, Command, Stdio};
-use std::os::unix::process::ExitStatusExt;
 
 const GITHUB_API_TOKEN: &'static str = env!("GITHUB_API_TOKEN");
 
+#[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
 enum DeploymentStatus {
+    Unknown,
     Pending,
-    Success,
     Error,
+    Success,
     Failure
+}
+
+impl<'a> From<&'a str> for DeploymentStatus {
+    fn from(origin: &str) -> Self {
+        match origin {
+            "pending" => DeploymentStatus::Pending,
+            "error" => DeploymentStatus::Error,
+            "success" => DeploymentStatus::Success,
+            "failure" => DeploymentStatus::Failure,
+            _ => DeploymentStatus::Unknown
+        }
+    }
 }
 
 impl fmt::Display for DeploymentStatus {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            &DeploymentStatus::Unknown => write!(f, "unknown"),
             &DeploymentStatus::Pending => write!(f, "pending"),
-            &DeploymentStatus::Success => write!(f, "success"),
             &DeploymentStatus::Error => write!(f, "error"),
+            &DeploymentStatus::Success => write!(f, "success"),
             &DeploymentStatus::Failure => write!(f, "failure")
         }
     }
 }
 
-struct Arguments<'a> {
-    repo: &'a str,
-    sha: &'a str,
-    command: &'a str
-}
-
 struct Deployment<'a> {
     id: Option<u64>,
     repo: &'a str,
-    sha: &'a str
+    head: &'a str,
+    base: Option<&'a str>
 }
 
 impl<'a> Deployment<'a> {
-    pub fn create(repo: &'a str, sha: &'a str) -> Self {
+    pub fn create(repo: &'a str, head: &'a str, base: Option<&'a str>) -> Self {
         let url = &format!("https://api.github.com/repos/{}/deployments", repo);
+
+        // TODO: Add payload in case of `base` given
         let payload = &format!(r#"{{
 "ref": "{}",
 "auto_merge": false,
 "environment": "production"
-}}"#, sha);
+}}"#, head);
 
         let response = post(url, payload).or_else(|e| {
             eprintln!("Failed to create new deployment: {:?}", e);
@@ -60,10 +70,10 @@ impl<'a> Deployment<'a> {
         let json = serde_json::from_str(&response).unwrap_or(Value::Null);
         let id = json.as_object().and_then(|o| { o["id"].as_u64() });
 
-        Deployment { id: id, repo: repo, sha: sha }
+        Deployment { id: id, repo: repo, head: head, base: base }
     }
 
-    pub fn update_status(&self, status: DeploymentStatus) -> Result<bool, &'static str> {
+    pub fn update_status(&self, status: &DeploymentStatus) -> Result<(), &'static str> {
         if self.id.is_none() { return Err("Unknown deployment ID") }
 
         let url = &format!(
@@ -80,7 +90,13 @@ impl<'a> Deployment<'a> {
         let json = serde_json::from_str(&response).unwrap_or(Value::Null);
         json.as_object().and_then(|o| { o["state"].as_str() })
             .ok_or("Failed find deployment status in response")
-            .map(|s| { s == status.to_string().as_str() })
+            .and_then(|s| {
+                if *status == DeploymentStatus::from(s) {
+                    Ok(())
+                } else {
+                    Err("Status didn't change after update")
+                }
+            })
     }
 }
 
@@ -113,63 +129,42 @@ pub fn post(url: &str, payload: &str) -> Result<String, &'static str> {
     String::from_utf8(buffer).or(empty_json_result)
 }
 
-fn run_command_and_exit(arguments: Arguments) {
-    let deployment = Deployment::create(arguments.repo, arguments.sha);
-
-    deployment.update_status(DeploymentStatus::Pending).unwrap_or_else(|e| {
-        eprintln!("Failed to update status [{}]: {}", DeploymentStatus::Pending, e);
-        false
-    });
-
-    let mut process = Command::new("bash")
-        .arg("-l")
-        .arg("-c")
-        .arg(arguments.command)
-        .stdin(Stdio::null())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .spawn()
-        .expect("Spawning command is failed to start");
-
-    let exit_status = process.wait().unwrap_or_else(|_| {
-        deployment.update_status(DeploymentStatus::Error).unwrap_or_else(|e| {
-            eprintln!("Failed to update status [{}]: {}", DeploymentStatus::Error, e);
-            false
-        });
-        ExitStatus::from_raw(1)
-    });
-
-    if exit_status.success() {
-        deployment.update_status(DeploymentStatus::Success).unwrap_or_else(|e| {
-            eprintln!("Failed to update status [{}]: {}", DeploymentStatus::Success, e);
-            false
-        });
-    } else {
-        deployment.update_status(DeploymentStatus::Failure).unwrap_or_else(|e| {
-            eprintln!("Failed to update status [{}]: {}", DeploymentStatus::Failure, e);
-            false
-        });
-    }
-
-    process::exit(match exit_status.code() {
-        Some(x) => x,
-        None => { eprintln!("Failed to get exit status of subprocess"); 1 }
-    })
-}
-
 fn main () {
     let matches = App::new("Github Deployment")
         .version("1.0")
         .author("Fedorov Sergey <sergey.fedorov@distribusion.com>")
-        .about("Wrap any command with github deployment updates")
-        .args_from_usage(
-            "-p --repo=[STRING] 'Github repository path as <owner>/<repo>'
-             -r --ref=[STRING] 'SHA hash of the deployed code'
-             -c --command=[STRING] 'Command to execute in /bin/bash -l'").get_matches();
+        .about("Create a new Github deployment and set its status")
+        .arg(
+            Arg::with_name("head").required(true).takes_value(true).long("head")
+                .help("The ref to deploy. This can be a branch, tag, or SHA")
+        ).arg(
+            Arg::with_name("base").required(false).takes_value(true).long("base").
+                help("The ref of previous deployment. This can be a branch, tag, or SHA")
 
-    let repo = matches.value_of("repo").expect("Please provide a github repo like <owner>/<repo>");
-    let sha = matches.value_of("ref").expect("Please provide a sha hash of the code");
-    let command = matches.value_of("command").expect("Please provide a command to execute");
+        ).arg(
+            Arg::with_name("status").required(true).takes_value(true).long("status")
+                .possible_values(&["pending", "error", "success", "failure"]) // FIXME: Take from enum
+                .default_value("pending") // FIXME: Take from enum
+                .help("A deployment status to be set")
+        ).arg(
+            Arg::with_name("repo").required(true)
+                .help("A Github repository path as <owner>/<repo>")
+        ).get_matches();
 
-    run_command_and_exit(Arguments { command: command, sha: sha, repo: repo })
+    let repo = matches.value_of("repo").unwrap();
+    let head = matches.value_of("head").unwrap();
+    let status = matches.value_of("status").unwrap();
+    let base = matches.value_of("base");
+
+    let deployment = Deployment::create(repo, head, base);
+    let deployment_status = DeploymentStatus::from(status);
+
+    if let Err(reason) = deployment.update_status(&deployment_status) {
+        eprintln!("Status update of deployment#{} to '{}' failed\nReason: {}",
+                  deployment.id.unwrap_or(0), deployment_status, reason);
+    } else {
+        println!("Status update of deployment#{} to '{}' succeed",
+                 deployment.id.unwrap_or(0), deployment_status);
+
+    }
 }
